4c55690033bd634653a0fa313885bc2c6e7d93ed
case <case@casesandberg.com>
2016-05-18 18:10:19 -0700

    eslint

--- src/flattenNames.js
+++ src/flattenNames.js
@@ -3,15 +3,16 @@
 import _ from 'lodash';
 
 export const flattenNames = (things) => {
-  var names = [];
+  const names = [];
 
   things.map((thing) => {
     _.isArray(thing) && flattenNames(thing).map((name) => names.push(name));
     _.isPlainObject(thing) && _.map(thing, (value, key) => {
       value === true && names.push(key);
-      names.push(key + '-' + value);
+      names.push(`${ key }-${ value }`);
     });
     _.isString(thing) && names.push(thing);
+    return thing;
   });
 
   return names;
--- src/mergeClasses.js
+++ src/mergeClasses.js
@@ -1,11 +1,13 @@
 'use strict';
 
 export const mergeClasses = (classes, activeNames) => {
-  let styles = classes.default && Object.assign({}, classes.default) || {};
+  const styles = classes.default && Object.assign({}, classes.default) || {};
   activeNames.map((name) => {
     if (classes[name] !== null) {
       Object.assign(styles, classes[name]);
     }
+
+    return name;
   });
   return styles;
 };
