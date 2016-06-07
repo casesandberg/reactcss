2214df070144283620487ee12631a7e6035a0c72
case <case@casesandberg.com>
2016-06-07 10:29:57 -0700

    Update Readme + Lib

--- gulp/tasks/javascript.js
+++ gulp/tasks/javascript.js
@@ -1,21 +1,21 @@
-import gulp from 'gulp';
-import uglify from 'gulp-uglify';
-import babel from 'gulp-babel';
-import coffee from 'gulp-cjsx';
+import gulp from 'gulp'
+import uglify from 'gulp-uglify'
+import babel from 'gulp-babel'
+import coffee from 'gulp-cjsx'
 
-import config from '../config';
+import config from '../config'
 
 gulp.task('js:dist', () => {
   return gulp.src(config.src.js)
     .pipe(babel())
     .pipe(uglify())
-    .pipe(gulp.dest(config.dist.dir));
-});
+    .pipe(gulp.dest(config.dist.dir))
+})
 
 gulp.task('js:modules', () => {
   return gulp.src('./modules/react-material-design/src/**/*.cjsx')
     .pipe(coffee())
     .pipe(babel())
     .pipe(uglify())
-    .pipe(gulp.dest('./modules/react-material-design/lib'));
-});
+    .pipe(gulp.dest('./modules/react-material-design/lib'))
+})
