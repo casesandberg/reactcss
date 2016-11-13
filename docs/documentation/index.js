5cb96eb379b43552ace8ca3fbf654b3cc24e9524
case <case@casesandberg.com>
2016-11-13 15:38:00 -0800

    Update Documentation And Add Examples (#136)

--- docs/documentation/index.js
+++ docs/documentation/index.js
@@ -1,4 +1,4 @@
-/* eslint global-require: 0 */
+/* eslint-disable global-require */
 
 module.exports = {
   '00': require('./intro.md'),
@@ -7,12 +7,10 @@ module.exports = {
   '00.03-activating-classes': require('./intro-activating-classes.md'),
   '01-react': require('./react.md'),
   '01.01-state-props': require('./react-state-props.md'),
-  '02-redux': require('./redux.md'),
-  '02.01-props': require('./redux-props.md'),
-  '03-react-native': require('./react-native.md'),
-  '03.01-props': require('./react-native-props.md'),
-  '04-autoprefixing': require('./autoprefixing.md'),
-  '05-hover': require('./hover.md'),
-  '06-pseudo-classes': require('./pseudo-classes.md'),
-  '07-media-queries': require('./media-queries.md'),
+  '02-react-native': require('./react-native.md'),
+  '03-autoprefixing': require('./autoprefixing.md'),
+  '04-hover': require('./hover.md'),
+  '05-pseudo-classes': require('./pseudo-classes.md'),
+  '06-media-queries': require('./media-queries.md'),
+  '07-examples': require('./examples.md'),
 }
--- docs/documentation/intro.md
+++ docs/documentation/intro.md
@@ -5,4 +5,4 @@ hideTitle: true
 ---
 *NEW IN 1.0.0*
 
-# Inline Styles in JS with support for [React](#react), [Redux](#redux), [React Native](#react-native), [Autoprefixing](#autoprefixing), [Hover](#hover), [Pseudo-Elements](#pseudo-elements) & [Media Queries](#media-queries)
+# Inline Styles in JS with support for [React](#react), [React Native](#react-native), [Autoprefixing](#autoprefixing), [Hover](#hover), [Pseudo-Elements](#pseudo-elements) & [Media Queries](#media-queries)
