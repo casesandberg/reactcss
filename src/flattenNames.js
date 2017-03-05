d42f23fe38fa3a22ddfbe074b96034c91ce9bc4c
case <case@casesandberg.com>
2017-03-04 21:16:08 -0800

    Make `flattenNames` more readable

--- src/flattenNames.js
+++ src/flattenNames.js
@@ -1,16 +1,19 @@
 import _ from 'lodash'
 
-export const flattenNames = (things) => {
+export const flattenNames = (things = []) => {
   const names = []
 
-  things.map((thing) => {
-    Array.isArray(thing) && flattenNames(thing).map(name => names.push(name))
-    _.isPlainObject(thing) && _.forOwn(thing, (value, key) => {
-      value === true && names.push(key)
-      names.push(`${ key }-${ value }`)
-    })
-    _.isString(thing) && names.push(thing)
-    return thing
+  _.map(things, (thing) => {
+    if (Array.isArray(thing)) {
+      flattenNames(thing).map(name => names.push(name))
+    } else if (_.isPlainObject(thing)) {
+      _.forOwn(thing, (value, key) => {
+        value === true && names.push(key)
+        names.push(`${ key }-${ value }`)
+      })
+    } else if (_.isString(thing)) {
+      names.push(thing)
+    }
   })
 
   return names
